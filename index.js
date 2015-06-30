f5621b4e07850b966f47c2b69feba86227e01255
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-06-30 07:42:25 -0400

    Always return a boolean value

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = isPromise;
 
 function isPromise(obj) {
-  return obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+  return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
 }
