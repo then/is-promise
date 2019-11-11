5648c303391ac44d4cca017eed5fe952abdf552c
掬一捧 <iliyang.cn@gmail.com>
2019-11-11 15:51:59 +0800

    add `instanceof Promise` check

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = isPromise;
 
 function isPromise(obj) {
-  return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+  return (Promise && (obj instanceof Promise)) || (!!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function');
 }
