19c6d89aa55f4ebd427f5707dfe2756ffe9080bc
Forbes Lindesay <forbes@lindesay.co.uk>
2020-04-26 14:22:12 +0100

    feat: add typescript definitions (#31)

---
+++ test-import/index.ts
@@ -0,0 +1,9 @@
+import * as ta from 'type-assertions';
+import isPromise from 'is-promise';
+
+declare const x: number | Promise<string>;
+if (isPromise(x)) {
+  ta.assert<ta.Equal<typeof x, Promise<string>>>();
+} else {
+  ta.assert<ta.Equal<typeof x, number>>();
+}
